0bec12a7abcf3f2109698edd397ea8335b3da038
Boyun Tang <tangboyun@hotmail.com>
2012-02-23 03:22:35 +0800

    finish 'predict' function

--- SVM/CSVM.hs
+++ SVM/CSVM.hs
@@ -1,5 +1,7 @@
-{-# LANGUAGE BangPatterns #-}
-{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE BangPatterns 
+            ,RankNTypes
+            ,FlexibleContexts
+ #-}
 -----------------------------------------------------------------------------
 -- |
 -- Module : 
@@ -21,16 +23,17 @@ import           Control.Exception
 import           Control.Monad
 import           Control.Monad.ST.Strict
 import           Control.Parallel.Strategies
-import           Data.Array.Repa
+import           Data.Array.Repa hiding (map)
 import qualified Data.IntMap                 as M
+import           Data.List                   (sort,group,sortBy)
 import qualified Data.Vector                 as V
 import qualified Data.Vector.Unboxed         as UV
 import qualified Data.Vector.Unboxed.Mutable as MV
+import           GHC.Conc
 import           SVM.Internal.Matrix
 import           SVM.Internal.SMO
 import           SVM.Kernel.Function
 import           SVM.Types
-import GHC.Conc                  
 
 defaultCSVMPara = SVMPara (RBF 0) 1 M.empty OVO
 
@@ -44,9 +47,9 @@ buildCSVM !f !dataSet !svmPara =
              in go $ RBF $! 1.0 / fromIntegral nFeature
     p     -> go p
   where 
-    go p' =
-        let mK = f dataSet p'
-        in SVM svmPara{kernelPara=p'} dataSet mK
+    go !p' =
+      let mK = f dataSet p'
+      in SVM svmPara{kernelPara=p'} dataSet mK
            
 train :: (RealFloat a,UV.Unbox a) => SVM a -> Model a
 train !svm = if (M.size . idxSlice . dataset $ svm) /= 2
@@ -57,6 +60,8 @@ train !svm = if (M.size . idxSlice . dataset $ svm) /= 2
              else trainOne svm
                     
 {-# INLINE trainOne #-}
+{-# SPECIALIZE trainOne :: SVM Double -> Model Double #-}
+{-# SPECIALIZE trainOne :: SVM Float -> Model Float #-}
 trainOne :: (RealFloat a,UV.Unbox a) => SVM a -> Model a
 trainOne !svm@(SVM p dat mK) = 
   let !y = labels $ dat
@@ -83,16 +88,20 @@ at = UV.unsafeIndex
 atV :: V.Vector a -> Int -> a
 atV = V.unsafeIndex    
 
+{-# INLINE encode #-}
 encode :: Int -> Int -> Int -> Int 
 encode !nClass !i !j = assert (j == -1 || i < j) $! nClass ^ (i - 1) + j
                     
+{-# INLINE decode #-}
 decode :: Int -> Int -> (Int,Int)
 decode !nClass !num = assert (nClass > 1) $! 
                       if num == 0 
                       then (1,-1)
                       else num `divMod` nClass
             
-
+{-# INLINE trainOVO #-}
+{-# SPECIALIZE trainOVO :: SVM Double -> Model Double #-}
+{-# SPECIALIZE trainOVO :: SVM Float -> Model Float #-}
 trainOVO :: (RealFloat a,UV.Unbox a) => SVM a -> Model a
 trainOVO !svm@(SVM p dat mK_orign) = 
   let !idxs = idxSlice dat
@@ -140,32 +149,39 @@ trainOVA = undefined
 trainDAG :: SVM a -> Model a
 trainDAG = undefined
 
-predict :: Model a -> Sample a -> Label
+{-# INLINE predict #-}
+{-# SPECIALIZE predict :: Model Double -> Sample Double -> Label #-}
+{-# SPECIALIZE predict :: Model Float -> Sample Float -> Label #-}
+predict :: (RealFloat a,UV.Unbox a) => Model a -> Sample a -> Label
 predict !(Model (SVM (SVMPara kP _ _ _) 
-                 (DataSet _ _ dat _) _) m) !sample = 
-  let !xs = M.assocs m
+                 (DataSet _ _ dat sli) _) m) !sample = 
+  let xs = M.assocs m
       f = kernel kP
-      !n = V.length sample
-  in assert (UV.length $ (sample `atV` 0) `at` 0 ==
-             UV.length $ (dat `atV` 0) `at` 0) $
+      n = V.length sample
+      nClass = M.size sli
+  in assert (UV.length (sample `atV` 0) ==
+             UV.length (dat `atV` 0)) $
      UV.fromList $! withStrategy 
      (parBuffer numCapabilities rdeepseq) $ map 
      (\idx_sample -> 
        let x = sample `atV` idx_sample
-                 = 
-             withStrategy 
-             (parBuffer numCapabilities rdeepseq) $ map
-             (\num (SVCoef r idxs sv_coef) ->
-               let !(i,j) = decode num
-                   !sum_dec = 
-                     UV.ifoldl'            
-                     (\acc idx coef ->
-                       let !x1 = dat `atV` (idxs `at` idx)
-                           !acc' = acc + 
-                                   UV.su
-                       in acc'
-                         ) 0.0 sv_coef
-               ) xs
-       in
-       ) [0..n-1]
+           final = head $ head $ 
+                sortBy (\a b -> compare (length b) (length a)) $ 
+                group $ sort $ withStrategy 
+                (parBuffer numCapabilities rdeepseq) $ map
+                (\(num,SVCoef r idxs sv_coef) ->
+                  let (i,j) = decode nClass num
+                      sum_dec = UV.ifoldl'            
+                                (\acc idx coef ->
+                                  let x1 = dat `atV` 
+                                            (idxs `at` idx)
+                                      acc' = acc + coef * 
+                                             (realToFrac (f x x1))
+                                   in acc') 0.0 sv_coef
+                      dec_v = sum_dec - r
+                      vote = if dec_v > 0 
+                             then i 
+                             else j
+                  in vote) xs
+       in final) [0..n-1]
             
--- SVM/Kernel/CPU.hs
+++ SVM/Kernel/CPU.hs
@@ -28,9 +28,9 @@ import           GHC.Conc                    (numCapabilities)
 import           SVM.Kernel.Function
 import           SVM.Types
 
+{-# INLINE kernelFunc #-}
 {-# SPECIALIZE kernelFunc :: DataSet Double -> KernelPara -> Matrix Double #-}
 {-# SPECIALIZE kernelFunc :: DataSet Float -> KernelPara -> Matrix Float #-}
-{-# INLINE kernelFunc #-}
 kernelFunc :: (RealFloat a,UV.Unbox a,NFData a) => 
               DataSet a -> KernelPara -> Matrix a
 kernelFunc !dataSet !kernelPara =
@@ -48,7 +48,7 @@ kernelFunc !dataSet !kernelPara =
                    let v_i = V.unsafeIndex sps i
                        v_j = V.unsafeIndex sps j
                        val = f v_i v_j
-                   in  (i,j,val))). splitEvery (ceiling $ 2000.0 / fromIntegral l))
+                   in  (i,j,val))). splitEvery (ceiling $! 2000.0 / fromIntegral l))
       vs1 = concat $ paraFunc ls1        
       vs2 = concat $ paraFunc ls2
       vec = UV.replicate (n*n) 0
