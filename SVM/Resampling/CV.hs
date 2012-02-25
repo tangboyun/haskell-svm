e8731c980fe054721248bdd87b669139fb2f384e
Boyun Tang <tangboyun@hotmail.com>
2012-02-26 04:25:25 +0800

    change name to CV

--- SVM/Resampling/CV.hs
+++ SVM/Resampling/CV.hs
@@ -11,9 +11,10 @@
 -- 
 --
 -----------------------------------------------------------------------------
-module SVM.Resampling.CrossValidation 
+module SVM.Resampling.CV 
        (
-         cvSplit
+         CVFold(..)
+       , cvSplit
        , cvSplit'
        )
        where
@@ -26,13 +27,61 @@ import SVM.Resampling.Shuffle
 import SVM.Types                 
 import Control.Monad.ST.Strict  
 import Control.Exception
+import Control.Parallel.Strategies
+import GHC.Conc
+import SVM.Internal.Matrix
+
+newtype CVFold = CVFold [(V.Vector Int,V.Vector Int)]
+
+-- kFoldCV_impl :: SVMPara -> DataSet a -> Matrix a -> CVFold -> Int
+-- kFoldCV_impl p dat mK (CVFold xs) =
+--   let !y = labels $ dat
+--       !yd = UV.map fromIntegral y
+--       !c = cost p
+--       !m = weight $! p
+--       !nClass = M.size $! idxSlice dat
+--       !mM = if nClass == 2
+--             then computeUnboxedP $ unsafeTraverse mK id 
+--                  (\f sh@(Z:.i:.j) ->
+--                    (realToFrac $ yd `atUV` i * yd `atUV` j) * 
+--                    (f sh))
+--             else mK
+--   in sum $ withStrategy 
+--      (parBuffer numCapabilities rdeepseq) $ 
+--      map (\(trainIdx,testIdx) ->
+--            let mM' = packKernel mM trainIdx
+--                y_train = UV.unsafeBackpermute y 
+--                          (UV.convert trainIdx)
+--                yd_train = UV.unsafeBackpermute yd trainIdx
+--                tQ' = packKernel2 mQ trainIdx testIdx
+--                sis = if nClass == 2
+--                      then [trainOneLowLevel 
+--                            (c*(m M.! 1)) 
+--                            (c*(m M.! -1)) 
+--                            y_train 
+--                            yd_train mM']
+--                      else 
+--                        let 
+--                          cs = [1..nClass]
+--                           withStrategy 
+--                             (parBuffer numCapabilities rdeepseq) $ 
+--                             map (\(i,j) ->
+--                                   let cP = c*(m M.! i)
+--                                       cN = c*(m M.! j)
+--                                       y' = UV.unsafeBackpermute 
+                                    
+--                                     ) $
+--                             concat [[(i,j)|j<-cs,j>i]|i<-cs]
+--            in 
+--            ) xs
+
 
 {-# INLINE cvSplit' #-}
-cvSplit' :: Int -> DataSet a -> [(V.Vector Int,V.Vector Int)]
+cvSplit' :: Int -> DataSet a -> CVFold
 cvSplit' = (fst .) . (cvSplit $ runST $ create >>= save)
 
 {-# INLINE cvSplit #-}
-cvSplit :: Seed -> Int -> DataSet a -> ([(V.Vector Int,V.Vector Int)],Seed)
+cvSplit :: Seed -> Int -> DataSet a -> (CVFold,Seed)
 cvSplit !s !kFold !(DataSet _ _ _ idxS) = assert (kFold > 0) $
   let !ls = M.elems idxS
       !nClass = M.size idxS
@@ -54,7 +103,7 @@ cvSplit !s !kFold !(DataSet _ _ _ idxS) = assert (kFold > 0) $
                                         V.ifilter (\idx _ -> idx /= k) e) cvVec
                  in (trainset,testset)
                ) [0..kFold-1]
-  in (ss,s'')
+  in (CVFold ss,s'')
   where 
     splitEvery len count vec = go count vec []
       where 
