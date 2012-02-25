1703aab7b4abd0ade016cd1a60c97af7966a28d8
Boyun Tang <tangboyun@hotmail.com>
2012-02-25 20:50:45 +0800

    add Benchmark for comparing performance with libsvm-binding

---
+++ Benchmark.hs
@@ -0,0 +1,14 @@
+{-# LANGUAGE BangPatterns #-}
+-----------------------------------------------------------------------------
+-- |
+-- Module : 
+-- Copyright : (c) 2012 Boyun Tang
+-- License : BSD-style
+-- Maintainer : tangboyun@hotmail.com
+-- Stability : experimental
+-- Portability : ghc
+--
+-- 
+--
+-----------------------------------------------------------------------------
+module Main where
